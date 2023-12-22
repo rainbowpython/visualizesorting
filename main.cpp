553642f5ed1c0401166b1e69fdfe22ddd4e720b5
rainbowpython <joshble@outlook.com>
2023-12-21 22:07:19 -0600

    fin

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <SFML/Audio.hpp>
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
     sf::RenderWindow window(sf::VideoMode(600,600), "uwu");
     sf::Event event;
     int width = 50;
@@ -22,11 +21,11 @@ int main(int argc, char const *argv[])
     for (size_t i = 0; i < arr.size(); i++){
             sf::RectangleShape rectangle(e);
             rectangles.push_back(rectangle);
-            //rectangles[i] = rectangle;
     }
-
-    while (window.isOpen())
-    {
+    int i = 0;
+    int j = 0;
+    bool change = false;
+    while (window.isOpen()){
 
         if (window.getSize() != v)
         {
@@ -40,44 +39,50 @@ int main(int argc, char const *argv[])
             }
         }
         window.clear();
-        
-            for (size_t j = 0; j < arr.size(); j++)
-            {
-                    for (size_t i = j; i < arr.size(); i++){
-                        if (clock.getElapsedTime().asSeconds() > 0.1){
-                        printf("iwi");
-                        height = arr[i];
-                        e.y = height;
-                        
-                        if (arr[j] > arr[i]) {
-                            int uwu = arr[j];
-                            arr[j] = arr[i];
-                            arr[i] = arr[j];
-                        }
-                        clock.restart();
-                        uwu.play();
+        if (clock.getElapsedTime().asSeconds() > 0.1){
+            //printf("e");
+            change = true;
+            if (j < arr.size()){
+                if(i < arr.size()){
+                    printf("iwi");
+                    height = arr[i];
+                    e.y = height;
+                    if (arr[j] > arr[i]) {
+                        int uwu = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = uwu;
                     }
-                
+                    clock.restart();
+                    i++;
+                }
+                else{
+                    i = j+1;
+                    j++;
+                }   
             }
-            //else{j--;}
-            
-        }
-        for (size_t i = 0; i < arr.size(); i++){
-            height = arr[i];
+        }   
+        
+        for (size_t k = 0; k < arr.size(); k++){
+            height = arr[k];
             e.y = height;
-            rectangles[i].setSize(e);
-            rectangles[i].setPosition(i*50, 600-height);
-            rectangles[i].setSize(e);
-            rectangles[i].setPosition(i*50, 600-height);
-                    
-            window.draw(rectangles[i]);
+            rectangles[k].setFillColor(sf::Color().White);
+            if (k == i && change){
+                rectangles[k].setFillColor(sf::Color().Blue);
+            }
+        
+            
+            rectangles[k].setSize(e);
+            rectangles[k].setPosition(k*50, 600-height);
+            rectangles[k].setSize(e);
+            rectangles[k].setPosition(k*50, 600-height);
+        
+            window.draw(rectangles[k]);
         }
         //uwu.play();
-        
-        
-
+        change = false;
         window.display();
     }
     
     return 0;
+    
 }
